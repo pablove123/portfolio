cd60d2b36560b6c4842ed1c1497f0e78378a4ec9
Pablo Villedae <pablov.e12345@gmail.com>
2023-01-29 12:03:39 -0800

    about page content done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,19 @@
 const About = (props) => {
   return ( 
-    <h1>About Page</h1>
+    <>
+    <h1>From cowboy to software developer</h1>
+    <img src="https://pbs.twimg.com/media/Fnqi_dfaUAA8Zan?format=jpg&name=small" alt="" />
+    <h2>I was raised on a cattle farm in a small town in Washington State. Raised by two immigrants from Mexico and carry my heritage with pride. Proudly LGBTQ+ and lover of all furry creatures</h2>
+    <h2>I am currently attending GA SEI Bootcamp. My aspiration in life is to make the world a better place everyday. Coding is an outlet for me to let my creative spirits flourish and allow me to be challenged every day. I am a University of Washington Graduate with double degrees in Political Science and Law, Society, and Justice.</h2>
+    <h3>Fun Facts About me:</h3>
+    <li>🇲🇽 First-Generational Mexican-American (fluent in both English and Spanish)</li>
+    <li>👨🏽‍🍳 Amatuer chef and baker</li>
+    <li>🎮 Professional Gamer</li>
+    <li>🦆 Favorite animal is a duck</li>
+    <li>💖💛 Pink and Yellow are my favorite colors</li>
+
+
+    </>
    );
 }
  
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
 const Home = (props) => {
   return ( 
-    <h1>Home page</h1>
+    <>
+    <h1>Welcome!</h1>
+    <img src="https://media.licdn.com/dms/image/C4E03AQG6Gz2988_ZlA/profile-displayphoto-shrink_200_200/0/1618382107352?e=1680739200&v=beta&t=oCjPHvguiOMXCmf72BNtoQWcPKLUpgaanCCrZRtN9QY" alt="" />
+    <h2>Pablo Villeda-Echeverria</h2>
+    <h3>Software Engineer</h3>
+    </>
    );
 }
  
