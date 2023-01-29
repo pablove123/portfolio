3ab576a9b4d793b6d338849f8349d77c721754ec
Pablo Villedae <pablov.e12345@gmail.com>
2023-01-29 11:46:28 -0800

    connected each page to nav bar

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,7 @@
+const Projects = (props) => {
+  return ( 
+    <h1>Project Page</h1>
+   );
+}
+ 
+export default Projects;
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,7 @@
+const Resume = (props) => {
+  return ( 
+    <h1>Resume Page</h1>
+   );
+}
+ 
+export default Resume;
