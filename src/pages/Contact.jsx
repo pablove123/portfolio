ad739f3d545746cb3d350630fe483f9b94f19758
Pablo Villedae <pablov.e12345@gmail.com>
2023-01-29 12:45:41 -0800

    add resume to resume page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,8 +3,8 @@ const Contact = (props) => {
     <>
     <h1>Contact Me!</h1>
     <h2>Email: <a href="mailto:contactpablove@gmail.com?subject=subject text">contactpablove@gmail.com</a></h2>
-    <h2><a target="_blank"  href="https://www.linkedin.com/in/pablove/">LinkedIn</a> </h2>
-    <h2><a target="_blank"  href="https://github.com/pablove123">Github</a></h2>
+    <h2><a rel="noreferrer" target="_blank"  href="https://www.linkedin.com/in/pablove/">LinkedIn</a> </h2>
+    <h2><a rel="noreferrer" target="_blank"  href="https://github.com/pablove123">Github</a></h2>
 
 
     
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,10 @@
 const Resume = (props) => {
   return ( 
-    <h1>Resume Page</h1>
+    <>
+    <h1><img src="https://pbs.twimg.com/media/FnqtRRTagAEicUO?format=jpg&name=small" alt="" /></h1>
+    <h2>Download my <a href="https://imgur.com/a/JfzMSas">Resume</a> </h2>
+    <img src="https://imgur.com/PZ75rR1" alt="" />
+    </>
    );
 }
  
