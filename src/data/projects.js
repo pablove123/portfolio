2b5c52ba865f3137fdafb16b8ef372def63d3e64
Pablo Villedae <pablov.e12345@gmail.com>
2023-01-29 14:35:51 -0800

    add unit projects

---
+++ src/data/projects.js
@@ -0,0 +1,26 @@
+const projectList=[
+  {title: "MyMoodyMonster", 
+  description:"A pet simulator web browser game where you care for a moody monster by monitoring its moods and earning its love in return.", 
+  image: "https://github.com/pablove123/MoodyMonster/raw/main/Pictures/screenshot.png", 
+  repositoryLink: "https://github.com/pablove123/MoodyMonster", 
+  deploymentLink: "https://mymoodymonster.netlify.app/"},
+
+  {title: "RecipeHub", 
+  description:"A web app where users can log in and post recipes, comment on others, and view and track their posts. ", 
+  image: "https://camo.githubusercontent.com/14dea76db304aaaded4bbff5524fb3a1ca594069c4373999c60d83bf829d9197/68747470733a2f2f7062732e7477696d672e636f6d2f6d656469612f466d304a567150615941417259524d3f666f726d61743d6a7067266e616d653d736d616c6c", 
+  repositoryLink: "https://github.com/pablove123/RecipeHub", 
+  deploymentLink: "https://recipehub.fly.dev/"},
+
+  {title: "TicTacToe", 
+  description:"Built with JavaScript", 
+  image: temp, 
+  repositoryLink: "https://github.com/pablove123/ttt-weekend", 
+  deploymentLink: temp},
+  {title: temp, 
+  description:temp, 
+  image: temp, 
+  repositoryLink: temp, 
+  deploymentLink: temp},
+]
+
+export default projectList
