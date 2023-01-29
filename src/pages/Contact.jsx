b9321b67b9a89139dbd1984d37250c2d53da080a
Pablo Villedae <pablov.e12345@gmail.com>
2023-01-29 12:31:14 -0800

    contact page content done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,14 @@
 const Contact = (props) => {
   return ( 
-    <h1>Contact Page</h1>
+    <>
+    <h1>Contact Me!</h1>
+    <h2>Email: <a href="mailto:contactpablove@gmail.com?subject=subject text">contactpablove@gmail.com</a></h2>
+    <h2><a target="_blank"  href="https://www.linkedin.com/in/pablove/">LinkedIn</a> </h2>
+    <h2><a target="_blank"  href="https://github.com/pablove123">Github</a></h2>
+
+
+    
+    </>
    );
 }
  
